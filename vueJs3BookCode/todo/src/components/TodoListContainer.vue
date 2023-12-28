5af9cd184b5022a39abf5267e61a407066943b2b
soyoung <makemeha2@gmail.com>
2023-12-28 14:47:40 +0900

    수정

--- vueJs3BookCode/todo/src/components/TodoListContainer.vue
+++ vueJs3BookCode/todo/src/components/TodoListContainer.vue
@@ -1,42 +1,69 @@
 <template>
-    <TodoList :data="todos" />
+    <todo-list-new />
+    <section class="container">
+        <div class="row justify-content-center m-2">
+            <todo-list-main />
+        </div>
+    </section>
 </template>
 
 <script>
 import { ref, readonly, provide } from 'vue'
-import TodoList from './TodoList.vue'
+import { useStorage } from '../compositions/storage';
+import TodoListNew from './TodoListNew.vue'
+import TodoListMain from './TodoListMain.vue'
 
 export default {
     name: 'TodoListContainer',
     setup(props) {
-        
         let tempTodos = [];
-        tempTodos.push({ job: '업무1', date: '2023-12-26', completed: false })
-        tempTodos.push({ job: '업무2', date: '2023-12-25', completed: false })
-        tempTodos.push({ job: '업무3', date: '2023-12-25', completed: false })
-        tempTodos.push({ job: '업무4', date: '2023-12-25', completed: true })
+        tempTodos.push({ job: '업무1', date: '2023-12-28', completed: false })
+        tempTodos.push({ job: '업무2', date: '2023-12-27', completed: false })
+        tempTodos.push({ job: '업무3', date: '2023-12-27', completed: false })
+        tempTodos.push({ job: '업무4', date: '2023-12-27', completed: true })
 
-        //const todos = ref([]);
         const todos = ref(tempTodos);
 
+        //const todos = ref([]);
+        const { loadTodos, saveTodos, storage_id } = useStorage();
         provide('todos', readonly(todos));
+
         const initTodos = (init_todos) => {
             todos.value = init_todos;
         }
-        const addTodo = (todo, date) => { }
-        const removeTodo = (id) => { }
-        const completeTodo = (id) => { }
+
+        const addTodo = (job, date) => { 
+            todos.value.push({
+                id: storage_id++,
+                job: job,
+                date: date,
+                completed: false,
+            })
+            saveTodos(todos)
+        }
+        
+        const removeTodo = (id) => { 
+            todos.value.splice(id, 1)
+            todos.value.forEach((todo, idx) => {
+                todo.id = idx;
+            })
+            saveTodos(todos);
+        }
+        
+        const completeTodo = (id) => {
+            todos.value.find((todo) => todo.id == id).completed = true;
+            saveTodos(todos)
+        }
 
         provide('addTodo', addTodo);
         provide('removeTodo', removeTodo);
         provide('completeTodo', completeTodo);
-
-        return {
-            todos
-        }
+        
+        loadTodos(initTodos);
     },
     components: {
-        TodoList
+        TodoListNew,
+        TodoListMain,
     }
 }
 </script>
